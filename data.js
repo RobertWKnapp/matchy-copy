348b2d742c15143f44aab595f9523962122d1919
RobertWKnapp <4609saratoga@gmail.com>
2024-07-22 16:17:27 -0500

    working on step 3

--- data.js
+++ data.js
@@ -11,20 +11,62 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Object Creation //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+/**
+ * ## Step 1 - Object Creation
+ 1. [ ] Open up the file data.js.
+ 2. [ ] Create a variable named `animal` and assign it to an empty object.
+ 3. [ ] Using **dot notation** give `animal` a **property** named `species` with a value of any animal species.
+ 4. [ ] Using **bracket notation** give `animal` a **property** called `name` with a value of your animal`s name.
+ 5. [ ] Using either notation, give `animal` a **property** called `noises` with a value of empty array.
+ 6. [ ] Print your `animal` Object to the console by adding, `console.log(animal);`,
+ 7. [ ] Save your work (command-s || ctrl-s), switch back to the browser tab with your website running, and refresh your page to see what `animal` looks like.
+ 8. [ ] It should be something like: 
+~~~JS
+{ species: 'duck', name: 'Jennifer', noises: [] }
+ */
+var animal = {}
+animal.species = 'Dog';
+animal["name"] = 'Puppy';
+animal.noises = [];
 
-
-
+console.log(animal);
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
+/**
+ * ## Step 2 - Array Creation
+ 1. [ ] Create a variable named `noises` and assign it to an empty array.
+ 2. [ ] Using **bracket notation** give `noises` it's first element. A string representing a sound your animal might make.
+ 3. [ ] Using an array function add another noise to the end of `noises`.
+ 4. [ ] Go to the array documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array?redirectlocale=en-US
+ 5. [ ] Look through the functions until you find the one that will place an element at the begining of the array.
+ 6. [ ] Add an element to `noises` using this function.
+ 7. [ ] Using **bracket syntax** again, add another element to the end of `noises`. Make sure that the way you do this step would work no matter how many elements `noises` had. In other words, don't hard code the position of the new element.
+ 8. [ ] `console.log` the length of `noises`
+ 9. [ ] `console.log` the last element in `noises` again without hard coding the index.
+ 10. [ ] `console.log` the whole array.
+ 11. [ ] Does it look right?
+ */
+var noises = [];
+noises[0] = 'woof';
+noises.push('yip');
+noises.unshift('growl');
+noises[noises.length] = "purr";
+console.log(noises.length);
+console.log(noises.length - 1);
+console.log(noises);
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
+/**## Step 3 - Combining Step 1 and 2
+ 1. [ ] Using **bracket syntax**, assign the `noises` property on `animal` to our new `noises` array.
+ 2. [ ] Using any syntax add another noise to the `noises` property on `animal`.
+ 3. [ ] `console.log` `animal`.
+ 4. [ ] Does it look right?
+ * 
+ */
+animal key value pair here
 
 /* *******************************************************************
  * Step 4 - Review
